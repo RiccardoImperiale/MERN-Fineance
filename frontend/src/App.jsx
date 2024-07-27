78d4c8c49be7b7ec2fcafedf3ca4f97a2c10859d
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-07-27 11:31:35 +0200

    add routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import LayoutAuth from "./layouts/LayoutAuth";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<LayoutAuth />}>
+        <Route index element={<Dashboard />} />
+        <Route path="/auth" element={<Auth />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
